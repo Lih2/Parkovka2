57e1a9e8ebaf7c5e05ed37f539162c9410102d63
Aleks <Lih2@yandex.ru>
2017-11-26 00:53:55 +0300

    Введение тарифов на парковку Ночь-день

--- src/Parkovka.java
+++ src/Parkovka.java
@@ -9,18 +9,36 @@ public class Parkovka {
 
     public static void main(String[] args) {
 
-        Parkovka parkovkatest = new Parkovka(5,2);
+
+        Parkovka parkovkatest = new Parkovka(5,2, new Tarif(7,22,4,2));
 
         ////// Test Variant 1
-        for(int i=0;i<5;i++)
-            parkovkatest.park(new Car(String.valueOf(i)),i*2);
+        Car car1=new Car("AAA");
+        Car car2=new Car("BBB");
+        Car car3=new Car("CCC");
+        Car car4=new Car("DDD");
 
-        for(int i=5;i<7;i++)
-            parkovkatest.park(new Truck(String.valueOf(i)),i*2);
+        Truck truck1=new Truck("EEE");
+        Truck truck2=new Truck("GGG");
 
-        for(int i=1000; i<1007; i++)
-            parkovkatest.unpark(String.valueOf(i) , i);
+        parkovkatest.park(car1,5);
+        parkovkatest.park(car2,6);
+        parkovkatest.park(car3,12);
+        parkovkatest.park(car4,22);
 
+        parkovkatest.park(truck1,6);
+        parkovkatest.park(truck2,10);
+
+
+        try {
+            float price = parkovkatest.unpark("AAA", 55);
+            price = parkovkatest.unpark("BBB", 8);
+            price = parkovkatest.unpark("CCC", 22);
+            price = parkovkatest.unpark("DDD", 52);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private int maxnumberCars;
@@ -29,9 +47,12 @@ public class Parkovka {
     private HashMap<String, Integer> carPlaces = new HashMap<>();
     private HashMap<String, Integer> truckPlaces = new HashMap<>();
 
-    public Parkovka(int maxnumberCars, int maxnumberTrucks ) {
+    private Tarif tarif;
+
+    public Parkovka(int maxnumberCars, int maxnumberTrucks, Tarif tarif ) {
         this.maxnumberCars=maxnumberCars;
         this.maxnumberTrucks=maxnumberTrucks;
+        this.tarif=tarif;
     }
 
     public synchronized boolean park(Transport transport, int startHours) {
@@ -60,18 +81,17 @@ public class Parkovka {
         return false;
 
     }
-    public synchronized int unpark(String carnumber, int hoursFinish ) {
+    public synchronized float unpark(String carnumber, int hoursFinish ) throws Exception {
 
         Integer hoursBegin = carPlaces.get(carnumber);
         if( hoursBegin == null  )
             hoursBegin = truckPlaces.get(carnumber);
-
-        if( hoursBegin == null ) return -100;
+        if( hoursBegin == null ) throw new Exception();
 
         carPlaces.remove(carnumber);
         truckPlaces.remove(carnumber);
 
-        return hoursFinish-hoursBegin;
+        return tarif.getTotalCost(hoursBegin, hoursFinish);
     }
 
 
@@ -94,6 +114,59 @@ public class Parkovka {
         public Truck(String number) { super(number);}
     }
 
+    public static class Tarif {
+
+        private int costDay,costNight;  // стоимость дневного и ночного тарифов
+        private int timeNight1, timeNight2; // время перехода тарифов , допустим 7 и 22
+
+        public Tarif(int timeNight1, int timeNight2, int costDay, int costNight) {
+            this.costDay=costDay;
+            this.costNight=costNight;
+            this.timeNight1=timeNight1;
+            this.timeNight2=timeNight2;
+        }
+
+        public float getTotalCost(int hoursBegin, int hoursFinish) throws Exception { // Расширенный метод под несколько суток
+
+            if(hoursFinish < hoursBegin) throw new Exception();
+            if( hoursBegin == hoursFinish) return 0;
+
+            int daysBegin=(int)(hoursBegin/24);
+            int daysFinish=(int)(hoursFinish/24);
+            int daysDiff = daysFinish - daysBegin; // сколько неполных суток
+
+            float sum=0;
+
+            if( daysDiff == 0 ) { // одни сутки
+                sum=getCostBetween(hoursBegin,hoursFinish );
+            }
+            else  if( daysDiff > 0 ) { // несколько суток
+                int restBegin=hoursBegin%24;
+                sum=sum+getCostBetween(restBegin,24 );
+                int restFinish=hoursFinish%24;
+                sum=sum+getCostBetween(0,restFinish );
+                sum=sum+getCostBetween(0,24 )*(daysDiff-1);
+            }
+
+            return sum;
+        }
+
+        public int getCostBetween(int hoursBegin, int hoursFinish ) throws Exception { // Метод для одних суток
+            if(hoursFinish < hoursBegin) throw new Exception();
+            int fin=getCostForHour(hoursFinish);
+            int beg=getCostForHour(hoursBegin);
+            return getCostForHour(hoursFinish) - getCostForHour(hoursBegin);
+        }
+        private int getCostForHour(int hour) { // Стоимость выезда с начала суток
+            if( hour <= timeNight1 )
+               return hour*costNight;
+            else if( hour > timeNight1 && hour <= timeNight2 )
+                return timeNight1*costNight + (hour-timeNight1)*costDay;
+            else
+                return timeNight1*costNight + (timeNight2-timeNight1)*costDay + (hour-timeNight2)*costNight;
+        }
+
+    }
 
 
 
